850d85bdccbbfcf907e211700b762008d68c555c
NewRokkie <ibrahim.gnamou@mindsprint.org>
2025-08-27 18:56:34 +0000

    Updated GateOut.tsx

--- src/components/Gates/GateOut.tsx
+++ src/components/Gates/GateOut.tsx
@@ -1051,7 +1051,7 @@ const GateOutCompletionModal: React.FC<{
                   title="Add Container (Max 2)"
                 >
                   <Plus className="h-4 w-4" />
-                  Add Container
+                  <span>Add Container</span>
                 </button>
               </div>
               
